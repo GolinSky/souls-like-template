3cc3d7df96d487ef33b588f2987625cf291625e8
GollinSky <golinsky97@gmail.com>
2026-08-10 18:56:32 +0300

    - refactor namings in animator component

--- Assets/Scripts/Components/Animator/AnimatorComponent.cs
+++ Assets/Scripts/Components/Animator/AnimatorComponent.cs
@@ -9,52 +9,52 @@ namespace SoulsLike.Entities.Character.Components
     /// <summary>
     /// Character animator component
     /// </summary>
-    public class AnimatorComponent: BaseComponent<AnimatorModel>, IInitializable,
+    public class AnimatorComponent : BaseComponent<AnimatorModel>, IInitializable,
         Animations.IObserver<AnimatorStateMachineDto>
     {
-        private static readonly int _animIdHorizontal = Animator.StringToHash("Horizontal");
-        private static readonly int _animIdVertical = Animator.StringToHash("Vertical");
-        private static readonly int _animIdGrounded = Animator.StringToHash("Grounded");
-        private static readonly int _animIdJump = Animator.StringToHash("Jump");
-        private static readonly int _animIdRoll = Animator.StringToHash("Roll");
-        private static readonly int _animIdBackStep = Animator.StringToHash("BackStep");
-        private static readonly int _animIdRollHorizontal = Animator.StringToHash("RollHorizontal");
-        private static readonly int _animIdRollVertical = Animator.StringToHash("RollVertical");
-        private static readonly int _animIdCrouch = Animator.StringToHash("Crouch");
-        private static readonly int _animIdTurn = Animator.StringToHash("Turn");
-        private static readonly int _animIdMoving = Animator.StringToHash("Moving");
-        private static readonly int _animIdSpeed = Animator.StringToHash("Speed");
-        private static readonly int _animIdLockOn = Animator.StringToHash("LockOn");
-        private static readonly int _lightAttackTrigger = Animator.StringToHash("LightAttack");
-        private static readonly int _lightAttackAltTrigger = Animator.StringToHash("LightAttackAlt");
-        private static readonly int _heavyAttackTrigger = Animator.StringToHash("HeavyAttack");
-        private static readonly int _chargedHeavyAttackTrigger = Animator.StringToHash("ChargedHeavyAttack");
-        private static readonly int _rollAttackTrigger = Animator.StringToHash("RollAttack");
-        private static readonly int _backStepAttackTrigger = Animator.StringToHash("BackStepAttack");
-        private static readonly int _runAttackTrigger = Animator.StringToHash("RunAttack");
-        private static readonly int _specialAttackTrigger = Animator.StringToHash("SpecialAttack");
+        private static readonly int AnimIdHorizontal = Animator.StringToHash("Horizontal");
+        private static readonly int AnimIdVertical = Animator.StringToHash("Vertical");
+        private static readonly int AnimIdGrounded = Animator.StringToHash("Grounded");
+        private static readonly int AnimIdJump = Animator.StringToHash("Jump");
+        private static readonly int AnimIdRoll = Animator.StringToHash("Roll");
+        private static readonly int AnimIdBackStep = Animator.StringToHash("BackStep");
+        private static readonly int AnimIdRollHorizontal = Animator.StringToHash("RollHorizontal");
+        private static readonly int AnimIdRollVertical = Animator.StringToHash("RollVertical");
+        private static readonly int AnimIdCrouch = Animator.StringToHash("Crouch");
+        private static readonly int AnimIdTurn = Animator.StringToHash("Turn");
+        private static readonly int AnimIdMoving = Animator.StringToHash("Moving");
+        private static readonly int AnimIdSpeed = Animator.StringToHash("Speed");
+        private static readonly int AnimIdLockOn = Animator.StringToHash("LockOn");
+        private static readonly int LightAttackTrigger = Animator.StringToHash("LightAttack");
+        private static readonly int LightAttackAltTrigger = Animator.StringToHash("LightAttackAlt");
+        private static readonly int HeavyAttackTrigger = Animator.StringToHash("HeavyAttack");
+        private static readonly int ChargedHeavyAttackTrigger = Animator.StringToHash("ChargedHeavyAttack");
+        private static readonly int RollAttackTrigger = Animator.StringToHash("RollAttack");
+        private static readonly int BackStepAttackTrigger = Animator.StringToHash("BackStepAttack");
+        private static readonly int RunAttackTrigger = Animator.StringToHash("RunAttack");
+        private static readonly int SpecialAttackTrigger = Animator.StringToHash("SpecialAttack");
 
         public void SetLockOn(bool isLockedOn)
         {
-            _animator.SetBool(_animIdLockOn, isLockedOn);
+            animator.SetBool(AnimIdLockOn, isLockedOn);
         }
         
-        [SerializeField] private Animator _animator;
-        [SerializeField] private AnimatorStateMachineReceiver _stateMachineReceiver;
+        [SerializeField] private Animator animator;
+        [SerializeField] private AnimatorStateMachineReceiver stateMachineReceiver;
         [Header("Aim Target")]
-        [SerializeField] private Transform _aimTarget;
+        [SerializeField] private Transform aimTarget;
         [Header("Aim Settings")]
-        [SerializeField] private float _aimTrackingSpeed = 10f;
-        [SerializeField] private float _aimVerticalOffset;
-        [SerializeField] private float _turningLagAmount = 5f;
+        [SerializeField] private float aimTrackingSpeed = 10f;
+        [SerializeField] private float aimVerticalOffset;
+        [SerializeField] private float turningLagAmount = 5f;
         [Header("Smooth Transitions")]
-        [SerializeField] private float _layerTransitionSpeed = 10f;
-        [SerializeField] private float _turnSmoothSpeed = 10f;
-        [SerializeField] private float _locomotionSmoothSpeed = 10f;
+        [SerializeField] private float layerTransitionSpeed = 10f;
+        [SerializeField] private float turnSmoothSpeed = 10f;
+        [SerializeField] private float locomotionSmoothSpeed = 10f;
+        [SerializeField] private AnimatorRootMotionRelay rootMotionRelay;
         
-        [field:SerializeField]public Transform RightHandAnchor { get; private set; }
+        [field:SerializeField] public Transform RightHandAnchor { get; private set; }
 
-    
         private float _currentTurnAmount;
         private float _targetSpeed;
         private float _currentSpeed;
@@ -64,64 +64,48 @@ namespace SoulsLike.Entities.Character.Components
         private float _targetRiffleLayerWeight;
         private bool _aimTargetInitialized;
         private IComponentMediator _mediator;
-        private AnimatorRootMotionRelay _rootMotionRelay;
         private bool _observingStateMachine;
-        public Animator Animator => _animator;
+        public Animator Animator => animator;
 
         public void Initialize()
         {
-            if (_animator == null)
-            {
-                throw new InvalidOperationException($"{name} requires an Animator.");
-            }
-
-            if (_stateMachineReceiver == null)
-            {
-                throw new InvalidOperationException($"{name} requires an AnimatorStateMachineReceiver.");
-            }
         }
 
         public void SetMediator(IComponentMediator mediator)
         {
             _mediator = mediator;
-            _rootMotionRelay = _animator.GetComponent<AnimatorRootMotionRelay>();
-            if (_rootMotionRelay == null)
-            {
-                _rootMotionRelay = _animator.gameObject.AddComponent<AnimatorRootMotionRelay>();
-            }
-
-            _rootMotionRelay.Initialize(mediator);
+            rootMotionRelay.Initialize(mediator);
             if (!_observingStateMachine)
             {
-                _stateMachineReceiver.AddObserver(this);
+                stateMachineReceiver.AddObserver(this);
                 _observingStateMachine = true;
             }
 
-            _animator.applyRootMotion = true;
+            animator.applyRootMotion = true;
         }
 
         public void SetGrounded(bool modelGrounded)
         {
-            _animator.SetBool(_animIdGrounded, modelGrounded);
+            animator.SetBool(AnimIdGrounded, modelGrounded);
         }
 
         public void PlayAttack(AttackType attackType)
         {
             int triggerHash = attackType switch
             {
-                AttackType.LightAttack => _lightAttackTrigger,
-                AttackType.LightAttackAlt => _lightAttackAltTrigger,
-                AttackType.RollingLightAttack => _rollAttackTrigger,
-                AttackType.SprintingAttack => _runAttackTrigger,
-                AttackType.HeavyAttack => _heavyAttackTrigger,
-                AttackType.ChargedHeavyAttack => _chargedHeavyAttackTrigger,
-                AttackType.SpecialAttack => _specialAttackTrigger,
-                AttackType.BackStepAttack => _backStepAttackTrigger,
+                AttackType.LightAttack => LightAttackTrigger,
+                AttackType.LightAttackAlt => LightAttackAltTrigger,
+                AttackType.RollingLightAttack => RollAttackTrigger,
+                AttackType.SprintingAttack => RunAttackTrigger,
+                AttackType.HeavyAttack => HeavyAttackTrigger,
+                AttackType.ChargedHeavyAttack => ChargedHeavyAttackTrigger,
+                AttackType.SpecialAttack => SpecialAttackTrigger,
+                AttackType.BackStepAttack => BackStepAttackTrigger,
                 _ => throw new ArgumentOutOfRangeException(nameof(attackType), attackType, null)
             };
 
             BeginRootMotionAction();
-            _animator.SetTrigger(triggerHash);
+            animator.SetTrigger(triggerHash);
         }
 
         public void UpdateState(AnimatorStateMachineDto state)
@@ -129,59 +113,50 @@ namespace SoulsLike.Entities.Character.Components
             _mediator.NotifyAnimatorStateChanged(state);
         }
 
-
         public void SetJump()
         {
-            _animator.SetTrigger(_animIdJump);
+            animator.SetTrigger(AnimIdJump);
         }
         
         public void TriggerRoll(Vector2 direction)
         {
-            _animator.SetFloat(_animIdRollHorizontal, direction.x);
-            _animator.SetFloat(_animIdRollVertical, direction.y);
+            animator.SetFloat(AnimIdRollHorizontal, direction.x);
+            animator.SetFloat(AnimIdRollVertical, direction.y);
             BeginRootMotionAction();
-            _animator.SetTrigger(_animIdRoll);
+            animator.SetTrigger(AnimIdRoll);
         }
 
         public void TriggerBackStep()
         {
             BeginRootMotionAction();
-            _animator.SetTrigger(_animIdBackStep);
+            animator.SetTrigger(AnimIdBackStep);
         }
 
         private void BeginRootMotionAction()
         {
-            if (_rootMotionRelay == null)
-            {
-                throw new InvalidOperationException($"{name} requires an AnimatorRootMotionRelay.");
-            }
-
-            _rootMotionRelay.BeginRootMotionContract();
+            rootMotionRelay.BeginRootMotionContract();
         }
 
         private void OnDestroy()
         {
             if (_observingStateMachine)
             {
-                _stateMachineReceiver.RemoveObserver(this);
+                stateMachineReceiver.RemoveObserver(this);
                 _observingStateMachine = false;
             }
         }
-        
 
         public void SetCrouch(bool isCrouching)
         {
-            _animator.SetBool(_animIdCrouch, isCrouching);
+            animator.SetBool(AnimIdCrouch, isCrouching);
         }
-        
-   
 
         public void SetAimTarget(Vector3 targetPosition)
         {
-            _targetAimPosition = targetPosition + Vector3.up * _aimVerticalOffset;
-            if (!_aimTargetInitialized && _aimTarget != null)
+            _targetAimPosition = targetPosition + Vector3.up * aimVerticalOffset;
+            if (!_aimTargetInitialized && aimTarget != null)
             {
-                _aimTarget.position = _targetAimPosition;
+                aimTarget.position = _targetAimPosition;
                 _aimTargetInitialized = true;
             }
         }
@@ -197,40 +172,38 @@ namespace SoulsLike.Entities.Character.Components
             float dt = Time.deltaTime;
 
             // 1. Smooth Aim Target Tracking with Turning Lag
-            if (_aimTarget != null && _aimTargetInitialized)
+            if (aimTarget != null && _aimTargetInitialized)
             {
                 // Calculate turning lag based on rotation velocity from movement
                 // We use transform.right to offset the target horizontally
-                float turnLag = _targetTurnAmount * _turningLagAmount; 
+                float turnLag = _targetTurnAmount * turningLagAmount; 
                 Vector3 laggedTarget = _targetAimPosition - transform.right * turnLag;
                 
-                _aimTarget.position = Vector3.Lerp(_aimTarget.position, laggedTarget, dt * _aimTrackingSpeed);
+                aimTarget.position = Vector3.Lerp(aimTarget.position, laggedTarget, dt * aimTrackingSpeed);
             }
-
 
             // 3. Smooth Animator Layer Weight
-            int riffleLayerIndex = _animator.GetLayerIndex("Riffle");
+            int riffleLayerIndex = animator.GetLayerIndex("Riffle");
             if (riffleLayerIndex != -1)
             {
-                float currentWeight = _animator.GetLayerWeight(riffleLayerIndex);
-                float nextWeight = Mathf.Lerp(currentWeight, _targetRiffleLayerWeight, dt * _layerTransitionSpeed);
-                _animator.SetLayerWeight(riffleLayerIndex, nextWeight);
+                float currentWeight = animator.GetLayerWeight(riffleLayerIndex);
+                float nextWeight = Mathf.Lerp(currentWeight, _targetRiffleLayerWeight, dt * layerTransitionSpeed);
+                animator.SetLayerWeight(riffleLayerIndex, nextWeight);
             }
 
-            _currentTurnAmount = Mathf.Lerp(_currentTurnAmount, _targetTurnAmount, dt * _turnSmoothSpeed);
-            _animator.SetFloat(_animIdTurn, _currentTurnAmount);
+            _currentTurnAmount = Mathf.Lerp(_currentTurnAmount, _targetTurnAmount, dt * turnSmoothSpeed);
+            animator.SetFloat(AnimIdTurn, _currentTurnAmount);
 
-            _currentSpeed = Mathf.Lerp(_currentSpeed, _targetSpeed, dt * _locomotionSmoothSpeed);
+            _currentSpeed = Mathf.Lerp(_currentSpeed, _targetSpeed, dt * locomotionSmoothSpeed);
             if (_currentSpeed < 0.01f) _currentSpeed = 0f;
-            _animator.SetFloat(_animIdSpeed, _currentSpeed);
+            animator.SetFloat(AnimIdSpeed, _currentSpeed);
 
-            _currentLocomotion = Vector2.Lerp(_currentLocomotion, _targetLocomotion, dt * _locomotionSmoothSpeed);
-            _animator.SetFloat(_animIdHorizontal, _currentLocomotion.x);
-            _animator.SetFloat(_animIdVertical, _currentLocomotion.y);
+            _currentLocomotion = Vector2.Lerp(_currentLocomotion, _targetLocomotion, dt * locomotionSmoothSpeed);
+            animator.SetFloat(AnimIdHorizontal, _currentLocomotion.x);
+            animator.SetFloat(AnimIdVertical, _currentLocomotion.y);
 
             bool isMoving = _targetSpeed > 0.01f || _targetLocomotion.sqrMagnitude > 0.0001f || _currentLocomotion.sqrMagnitude > 0.0001f;
-            _animator.SetBool(_animIdMoving, isMoving);
-
+            animator.SetBool(AnimIdMoving, isMoving);
         }
         
         private float _targetTurnAmount;
@@ -291,26 +264,13 @@ namespace SoulsLike.Entities.Character.Components
                 }
             }
         }
-        
-        
+
         private void OnFootstep(AnimationEvent animationEvent)
         {
-            // if (animationEvent.animatorClipInfo.weight > 0.5f)
-            // {
-            //     if (FootstepAudioClips.Length > 0)
-            //     {
-            //         var index = Random.Range(0, FootstepAudioClips.Length);
-            //         AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(_controller.center), FootstepAudioVolume);
-            //     }
-            // }
         }
 
         private void OnLand(AnimationEvent animationEvent)
         {
-            // if (animationEvent.animatorClipInfo.weight > 0.5f)
-            // {
-            //     AudioSource.PlayClipAtPoint(LandingAudioClip, transform.TransformPoint(_controller.center), FootstepAudioVolume);
-            // }
         }
     }
 }
